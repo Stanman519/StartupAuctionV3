dc90f439226d3452f28b053417b37ec3319435b7
Stanman519 <Stanman519@gmail.com>
2020-08-26 11:31:29 -0400

    took away preauthorize because it is not working yet

--- backend/src/main/java/com/stanfan/StartupAuctionV3/controller/AuctionController.java
+++ backend/src/main/java/com/stanfan/StartupAuctionV3/controller/AuctionController.java
@@ -144,14 +144,12 @@ public class AuctionController {
 	}
 	
 	//add owner to player when lot is won
-	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(path = "/api/win/player/", method = RequestMethod.PUT)
 	public void winAuction(@RequestBody @PathVariable int playerid, Owner winningOwner) {
 		playerDAO.addOwnerToPlayer(playerid, winningOwner.getOwnerId());
 	}
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(path = "/api/nominate", method = RequestMethod.PUT)
 	public void tempOwnerForNomination(@RequestBody Player player) {
 		playerDAO.addOwnerToPlayer(player.getPlayerId(), player.getOwnerId());
@@ -162,7 +160,7 @@ public class AuctionController {
 	public void updatePlayerAfterWin(@RequestBody Bid bid) {
 		playerDAO.addInfoAfterWin(bid);
 	}
-	@PreAuthorize("isAuthenticated()")
+
 	@CrossOrigin(origins = "http://localhost:8081", allowedHeaders = "*")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@RequestMapping(path = "/api/win/owner", method = RequestMethod.PUT)
